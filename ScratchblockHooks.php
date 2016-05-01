2950bd80cbe2eabdc349cc79b661fb1215273d77
Choco31415 <Eparke31415@gmail.com>
2016-05-01 17:38:42 -0400

    More experimental!

--- ScratchblockHooks.php
+++ ScratchblockHooks.php
@@ -7,8 +7,8 @@ class ScratchblockHook {
 	
 	public static function sbParserInit (Parser $parser) {
 		// Register <scratchblocks> and <sb> tag
-		$parser->setHook('scratchblocks', 'ScratchblockHooks::sbRenderTag');
-		$parser->setHook('sb', 'ScratchblockHooks::sbRenderInlineTag');
+		$parser->setHook('scratchblocks', 'ScratchblockHook::sbRenderTag');
+		$parser->setHook('sb', 'ScratchblockHook::sbRenderInlineTag');
 		return true;
 	}
 	 
@@ -16,6 +16,7 @@ class ScratchblockHook {
 	// Ouput HTML for <scratchblocks> tag
 
 	public static function sbRenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
+		throw new Exception("234");
 		return '<pre class="blocks">' . htmlspecialchars($input) . '</pre>';
 	}
 
@@ -23,6 +24,7 @@ class ScratchblockHook {
 
 	public static function sbRenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
 		//throw new Exception("what");
+		throw new Exception("2345");
 		return '<code class="blocks">' . htmlspecialchars($input) . '</code>';
 	}
 }
